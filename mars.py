80fa31728682f9b31c9a7be6cee2217671750d1c
Hal <halley@libaui.com>
2017-01-19 15:39:52 -0600

    Add files via upload

--- mars.py
+++ mars.py
@@ -7,12 +7,13 @@ def print_slow(str):
 playername = str(input("What is your name? "))
 print('')
 print_slow('Welcome to the Martian Trail, %s.\n\n' % playername)
-time.sleep(1) # delays for 2 seconds
+time.sleep(1) # delays for 1 seconds
 
 print_slow("In this game, you are going to plan and control a mission to Mars. \n\n")
-time.sleep(1) # delays for 2 seconds
+time.sleep(1) # delays for 1 seconds
 print_slow("How many rockets do you want to launch? ")
 rocketcount = int(input(""))
 print('')
-time.sleep(1) # delays for 2 seconds
+time.sleep(1) # delays for 1 seconds
 print_slow("Cool.  %s rockets. \n" % rocketcount)
+print_slow("The end. \n" % rocketcount)
